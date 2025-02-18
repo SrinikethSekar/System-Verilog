fdc686f43608867539a0945c837f862a32e75a1a
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-02-18 16:11:14 +0530

    Create array_assignment.sv

---
+++ Day 1/array_assignment.sv
@@ -0,0 +1,9 @@
+
+module foreach_example;
+  int array[5] = '{100, 200, 300, 400, 500};
+  initial begin
+    foreach (array[i]) begin
+      $display("array[%0d] = %0d", i, array[i]);
+    end
+  end
+endmodule
