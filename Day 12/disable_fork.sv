80884b52f37d64054ec37c60b381a0ffd93587a7
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-03-21 22:57:12 +0530

    Create disable_fork.sv

---
+++ Day 12/disable_fork.sv
@@ -0,0 +1,41 @@
+module disable_fork;
+
+	initial begin
+
+	fork
+		begin
+			
+			$display("Process A started at time=%0t",$time);
+			#10;
+			$display("Process A completed at time=%0t",$time);
+
+		end
+		begin
+			#2;
+			$display("Process B started at time =%0t",$time);
+			#2;
+			$display("Process B completed at time =%0t",$time);
+
+		end 
+		begin
+			#3;
+			$display("Process C started at time=%0t",$time);
+			#15;
+			$display("Process C completed at time=%0t",$time);
+
+		end 
+		join_any
+		disable fork;
+		$display("Fork join any completed at time=%0t",$time);
+	
+
+	end 
+
+endmodule
+
+//OUTPUT
+// # Process A started at time=0
+// # Process B started at time =2
+// # Process C started at time=3
+// # Process B completed at time =4
+// # Fork join any completed at time=4
