5a269bd35b4dff4e2e498a46634190cba440f3be
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-03-24 16:39:56 +0530

    Create random_case.sv

---
+++ Day 14/random_case.sv
@@ -0,0 +1,39 @@
+module r_case;
+
+  initial begin   
+
+    $display("INDIAN CRICKET");
+    $display("ALAPARAI");
+    
+    for (int i = 0; i < 6; i++) begin
+
+      randcase
+
+        0: $display("\t \n Thala sons"); 
+        1: $display("\t \n Virat Veriyans"); 
+        2: $display("\t \n Rohit Fans muttu"); 
+        3: $display("\t \n Gambir fans credit");
+      
+      endcase
+
+    end
+
+  end
+
+endmodule
+
+
+// INDIAN CRICKET
+// ALAPARAI
+   
+//  Rohit Fans muttu
+   
+//  Gambir fans credit
+   
+//  Virat Veriyans
+   
+//  Gambir fans credit
+   
+//  Gambir fans credit
+   
+//  Gambir fans credit
